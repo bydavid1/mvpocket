b2c98a1bcdafa8dc149be34c2d6f999da4afa147
Byron Jimenez <byronjimenez9911@gmail.com>
2020-08-25 20:56:23 -0600

    JWT Authentication

--- routes/api.php
+++ routes/api.php
@@ -16,16 +16,38 @@ use Illuminate\Support\Facades\Route;
 
 //Auth
 
-Route::post('auth/register', 'AuthController@register');
 
-Route::get('collection' , 'CollectionController@index');
-Route::post('collection/store' , 'CollectionController@store');
-Route::get('collection/edit/{id}' , 'CollectionController@edit');
-Route::put('collection/update/{id}' , 'CollectionController@update');
-Route::delete('collection/delete/{id}' , 'CollectionController@destroy');
 
-Route::get('collection/{id}/sites' , 'SiteController@index');
-Route::get('fetchOpenGraph' , 'SiteController@getOpenGraph');
-Route::post('collection/{id}/site/store' , 'SiteController@store');
-Route::put('collection/site/update/{id}', 'SiteController@update');
-Route::put('collection/site/delete/{id}', 'SiteController@destroy');
+Route::prefix('auth')->group(function () {
+
+    // Below mention routes are public, user can access those without any restriction.
+    // Create New User
+    Route::post('register', 'AuthController@register');
+
+    // Login User
+    Route::post('login', 'AuthController@login');
+    
+    // Refresh the JWT Token
+    Route::get('refresh', 'AuthController@refresh');
+    
+    // Below mention routes are available only for the authenticated users.
+    Route::middleware('auth:api')->group(function () {
+        // Get user info
+        Route::get('user', 'AuthController@user');
+
+        // Logout user from application
+        Route::post('logout', 'AuthController@logout');
+
+        Route::get('collection' , 'CollectionController@index');
+        Route::post('collection/store' , 'CollectionController@store');
+        Route::get('collection/edit/{id}' , 'CollectionController@edit');
+        Route::put('collection/update/{id}' , 'CollectionController@update');
+        Route::delete('collection/delete/{id}' , 'CollectionController@destroy');
+
+        Route::get('collection/{id}/sites' , 'SiteController@index');
+        Route::get('fetchOpenGraph' , 'SiteController@getOpenGraph');
+        Route::post('collection/{id}/site/store' , 'SiteController@store');
+        Route::put('collection/site/update/{id}', 'SiteController@update');
+        Route::put('collection/site/delete/{id}', 'SiteController@destroy');
+    });
+});
